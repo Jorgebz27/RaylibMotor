5f45468235bdcdaa2b83e36ff3417bf50c9e120e
Jorgebz27 <zenit15george@gmail.com>
2025-03-14 22:19:31 -0600

    Ya funciona lua

--- src/main.cpp
+++ src/main.cpp
@@ -9,6 +9,7 @@ by Jeffery Myers is marked with CC0 1.0. To view a copy of this license, visit h
 
 #include "raylib.h"
 #include "rlgl.h"
+#include "lua.hpp"
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
@@ -157,29 +158,124 @@ void DrawCubeTexture(Texture2D texture, Vector3 position, float width, float hei
     rlSetTexture(0);
 }
 
+
+//////////////////////////
+//Hacer minimo dos figuras de raylib llamadas con LUA
+//////////////////////////
+
+int DrawCircle(lua_State* L)
+{
+    float x = (float)lua_tonumber(L, 1);
+    float y = (float)lua_tonumber(L, 2);
+    float radius = (float)lua_tonumber(L, 3);
+    float r = (float)lua_tonumber(L, 4);
+    float g = (float)lua_tonumber(L, 5);
+    float b = (float)lua_tonumber(L, 6);
+    Color color = { r, g, b, 255 };
+    DrawCircle(x, y, radius, color);
+    return 0;
+}
+
+int DrawRectangle(lua_State* L)
+{
+    float x = (float)lua_tonumber(L, 1);
+    float y = (float)lua_tonumber(L, 2);
+    float w = (float)lua_tonumber(L, 3);
+    float h = (float)lua_tonumber(L, 4);
+    float r = (float)lua_tonumber(L, 5);
+    float g = (float)lua_tonumber(L, 6);
+    float b = (float)lua_tonumber(L, 7);
+    Color color = { r, g, b, 255 };
+    DrawRectangle(x, y, w, h, color);
+    return 0;
+}
+
+int Clear(lua_State* L)
+{
+    int r = (float)lua_tonumber(L, 1);
+    int g = (float)lua_tonumber(L, 2);
+    int b = (float)lua_tonumber(L, 3);
+    int a = (float)lua_tonumber(L, 4);
+
+    Color c = { r, g, b, a };
+
+    ClearBackground(c);
+
+    return 0;
+}
+
+void luaDraw(lua_State* L, float dt)
+{
+    lua_getglobal(L, "Draw");
+    if (lua_isfunction(L, -1))
+    {
+        //Log(L"Calling draw function from lua");
+        lua_pushnumber(L, dt);
+        if (lua_pcall(L, 1, 0, 0) != 0)
+        {
+            printf("Error calling draw function from lua");
+            printf(lua_tostring(L, -1));
+        }
+    }
+    else
+    {
+        printf("Draw function not found in lua");
+    }
+}
+
+int lua_mymodule(lua_State* L)
+{
+    static const luaL_Reg myModule[] =
+    {
+    { "Clear", Clear },
+    { "DrawCircle", DrawCircle },
+    { "DrawRect", DrawRectangle },
+    { NULL, NULL }
+    };
+    luaL_newlib(L, myModule);
+    return 1;
+}
+
 int main(int argc, char** argv)
 {
 
     //NO FUNCIONA
-	//WebRequestTest webReq;
-	//webReq.testWebLua();
+    //WebRequestTest webReq;
+    //webReq.testWebLua();
 
-	// Tell the window to use vsync and work on high DPI displays
-	SetConfigFlags(FLAG_VSYNC_HINT | FLAG_WINDOW_HIGHDPI);
+    // Tell the window to use vsync and work on high DPI displays
+    SetConfigFlags(FLAG_VSYNC_HINT | FLAG_WINDOW_HIGHDPI);
+
+    //// Initialize Lua
+    lua_State* L = luaL_newstate();
+    luaL_openlibs(L);
+    luaL_requiref(L, "SimpleDraw", lua_mymodule, 1);
+    lua_pop(L, 1);
+
+    if (luaL_dofile(L, "main.lua"))
+    {
+        printf("Error cargando el script main.lua");
+        printf(lua_tostring(L, -1));
+    }
+    else
+        printf("main.lua file loaded\n");
 
     VideoConfig config = { 640, 480, 0, 1 };  // valores por defecto
     LoadConfig(&config);
 
-    if (config.vsync) {
-        SetConfigFlags(FLAG_VSYNC_HINT);
-    }
-    if (config.fullscreen) {
-        SetConfigFlags(FLAG_FULLSCREEN_MODE);
-    }
+    FILE* configFile = fopen("config.ini", "r");
+    if (configFile == NULL)
+        printf("No se pudo abrir el archivo config.ini\n");
+    else
+        printf("Archivo config.ini abierto\n");
+    fclose(configFile);
 
-	// Create the window and OpenGL context
+    if (config.vsync) SetConfigFlags(FLAG_VSYNC_HINT);
+    if (config.fullscreen) SetConfigFlags(FLAG_FULLSCREEN_MODE);
+
+    // Create the window and OpenGL context
     InitWindow(config.resX, config.resY, "GameEngine");
-    //SetWindowState(FLAG_FULLSCREEN_MODE);
+    if (config.fullscreen) ToggleFullscreen();
 
     GameObject* go = new GameObject();
     ptrComponent newComp = std::make_shared<CubeComponent>();
@@ -194,7 +290,7 @@ int main(int argc, char** argv)
         gameobjects.push_back(go);
     }
 
-	MemoryManager::getInstance()->alloc(2*1024*1024*1024);
+    MemoryManager::getInstance()->alloc(2 * 1024 * 1024 * 1024);
 
     SetLogLevel(L_DEBUG);
 
@@ -206,93 +302,70 @@ int main(int argc, char** argv)
 
     printf("Initializing game subsystem");
 
-	// Utility function from resource_dir.h to find the resources folder and set it as the current working directory so we can load from it
-	SearchAndSetResourceDir("resources");
+    // Utility function from resource_dir.h to find the resources folder and set it as the current working directory so we can load from it
+    SearchAndSetResourceDir("resources");
 
-    //GameObject* go = new GameObject();
+    // Load a texture from the resources directory
+    Texture wabbit = LoadTexture("wabbit_alpha.png");
+    Texture cube = LoadTexture("texturacubo.png");
 
-	// Load a texture from the resources directory
-	Texture wabbit = LoadTexture("wabbit_alpha.png");
-	Texture cube = LoadTexture("texturacubo.png");
 
-    //Modelo 3d con textura
-    //Model model = LoadModel("resources/Angry_Bird_Red.obj");
-    //Texture2D texture = LoadTexture("resources/Angry_Bird.png");
-    //model.materials[0].maps[MATERIAL_MAP_DIFFUSE].texture = texture;
-    //Vector3 position = { 0.0f, 0.0f, 0.0f };
-
-	Camera3D camera = {0};
-	camera.position = { 4,0,2 };
-	camera.target = { 0,0,0 };
-	camera.up = { 0,1,0 };
-	camera.fovy = 120;
-	camera.projection = CAMERA_PERSPECTIVE;
+    Camera3D camera = { 0 };
+    camera.position = { 4,0,2 };
+    camera.target = { 0,0,0 };
+    camera.up = { 0,1,0 };
+    camera.fovy = 120;
+    camera.projection = CAMERA_PERSPECTIVE;
 
     AudioManager::getInstance()->LoadbgMusic("TECNO1.XM");
-	AudioManager::getInstance()->PlaybgMusic();
-	
-	// game loop
-	while (!WindowShouldClose())		// run the loop untill the user presses ESCAPE or presses the Close button on the window
-	{
-		AudioManager::getInstance()->Update();
+    AudioManager::getInstance()->PlaybgMusic();
 
-		UpdateCamera(&camera, CAMERA_FREE);
-		// drawing
+    // game loop
+    while (!WindowShouldClose())		// run the loop untill the user presses ESCAPE or presses the Close button on the window
+    {
+        AudioManager::getInstance()->Update();
 
-		//for (int i = 0; i < gameObjects.size(); i++) 
-        //{
-        //    if(gameObjects[i]->enabled)
-		//	    gameObjects[i]->update();
-		//}
-        //go->Update(GetFrameTime());
+        UpdateCamera(&camera, CAMERA_FREE);
+        // drawing
 
         for (auto& go : gameobjects)
         {
             go->Update(GetFrameTime());
         }
 
-		BeginDrawing();
+        BeginDrawing();
 
+        // Setup the back buffer for drawing (clear color and depth buffers)
+        ClearBackground(BLACK);
 
-		//go->Draw(GetFrameTime());
-
-		// Setup the back buffer for drawing (clear color and depth buffers)
-		ClearBackground(BLACK);
-
-		BeginMode3D(camera);
-		//DrawCube((Vector3) { 0, 0, 0 }, 1, 1, 1, RED);
+        BeginMode3D(camera);
 
         for (auto& go : gameobjects)
         {
             go->Draw(GetFrameTime());
         }
 
-        DrawCubeTexture(cube, {0, 0, 0}, 2, 2, 2, WHITE);
+        DrawCubeTexture(cube, { 0, 0, 0 }, 2, 2, 2, WHITE);
         //DrawModel(model, position, 2.0f, WHITE);
 
-		DrawGrid(20, 10);
-		//// draw some text using the default font
-		//DrawText("Hello Raylib", 200,200,20,WHITE);
-		EndMode3D();
-		//// draw our texture to the screen
-		DrawTexture(wabbit, 400, 200, WHITE);
+        DrawGrid(20, 10);
+        //// draw some text using the default font
+        //DrawText("Hello Raylib", 200,200,20,WHITE);
+        EndMode3D();
+        //// draw our texture to the screen
+        DrawTexture(wabbit, 400, 200, WHITE);
+        //}
+        luaDraw(L, GetFrameTime());
+        // end the frame and get ready for the next one  (display frame, poll input, etc...)
+        EndDrawing();
+    }
 
-		//for (int i = 0; i < gameObjects.size(); i++) {
-		//	gameObjects[i]->draw();
-		//}
-
-		// end the frame and get ready for the next one  (display frame, poll input, etc...)
-		EndDrawing();
-	}
-
-	// cleanup
-	// unload our texture so it can be cleaned up
-	UnloadTexture(wabbit);
+    // cleanup
+    // unload our texture so it can be cleaned up
+    UnloadTexture(wabbit);
     UnloadTexture(cube);
-    //UnloadTexture(texture);
-    //UnloadModel(model);
 
-	// destroy the window and cleanup the OpenGL context
-	CloseWindow();
-	return 0;
+    // destroy the window and cleanup the OpenGL context
+    CloseWindow();
+    return 0;
 }
